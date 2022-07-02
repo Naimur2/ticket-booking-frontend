bb3282cbb7faae5988130665ef41d8211a937b45
Naimur Rahaman <naimurrahaman248@gmail.com>
2022-07-02 14:38:12 +0600

    added sass

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import NotFound from "./pages/404/404";
+import Login from "./pages/Auth/Login/Login";
+import Register from "./pages/Auth/Register/Register";
 import NavBar from "./pages/common/NavBar";
 import Home from "./pages/User/Home/Home";
 
@@ -10,6 +12,8 @@ export default function App() {
             <NavBar />
             <Routes>
                 <Route path="/" element={<Home />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
                 <Route path="*" element={<NotFound />} />
             </Routes>
         </div>
